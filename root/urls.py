2e9a0bbce883588d068c70b1917c5824e5c9f92c
islamnurdinov <nurdinovislam4@gmail.com>
2024-08-29 18:38:08 +0600

    daa

--- root/urls.py
+++ root/urls.py
@@ -14,9 +14,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
+from root import settings
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="API documentation for my project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myproject.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+                  path('admin/', admin.site.urls),
+                  path('api_user/', include('user.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
