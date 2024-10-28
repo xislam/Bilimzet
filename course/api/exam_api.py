2210fe076c6780b7233f17b37d75808a6eef7a35
islamnurdinov <nurdinovislam4@gmail.com>
2024-10-28 19:48:55 +0600

    daa

--- course/api/exam_api.py
+++ course/api/exam_api.py
@@ -14,60 +14,6 @@ from course.serializers.exam import ExamDetailSerializer, CertificateSerializer
 class ExamResultsViewSet(viewsets.ViewSet):
     authentication_classes = (JWTAuthentication,)
 
-    @swagger_auto_schema(
-        operation_description="Submit exam results and receive a certificate if the score is sufficient.",
-        request_body=openapi.Schema(
-            type=openapi.TYPE_OBJECT,
-            properties={
-                'user_answers': openapi.Schema(
-                    type=openapi.TYPE_ARRAY,
-                    items=openapi.Schema(
-                        type=openapi.TYPE_OBJECT,
-                        properties={
-                            'question_id': openapi.Schema(type=openapi.TYPE_INTEGER),
-                            'selected_answer_id': openapi.Schema(type=openapi.TYPE_INTEGER),
-                        }
-                    ),
-                )
-            },
-            required=True,
-        ),
-        responses={
-            201: openapi.Response(
-                'Successful Response',
-                openapi.Schema(
-                    type=openapi.TYPE_OBJECT,
-                    properties={
-                        'correct_answers': openapi.Schema(type=openapi.TYPE_INTEGER),
-                        'incorrect_answers': openapi.Schema(type=openapi.TYPE_INTEGER),
-                        'total_questions': openapi.Schema(type=openapi.TYPE_INTEGER),
-                        'score': openapi.Schema(type=openapi.TYPE_INTEGER),
-                        'certificate_issued': openapi.Schema(type=openapi.TYPE_BOOLEAN),
-                    }
-                )
-            ),
-            400: "Bad Request",
-            404: "Not Found",
-        },
-        examples={
-            'application/json': {
-                "user_answers": [
-                    {
-                        "question_id": 1,
-                        "selected_answer_id": 2
-                    },
-                    {
-                        "question_id": 2,
-                        "selected_answer_id": 3
-                    },
-                    {
-                        "question_id": 3,
-                        "selected_answer_id": 1
-                    }
-                ]
-            }
-        }
-    )
     def create(self, request, exam_id):
         exam = Exam.objects.get(id=exam_id)
         user_answers = request.data.get('user_answers')  # Ожидаем список словарей с вопросами и ответами
