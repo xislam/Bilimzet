0da382779c66b36665935d30d443eff8fcf35543
islamnurdinov <nurdinovislam4@gmail.com>
2024-11-06 02:57:07 +0600

    daa

--- root/settings.py
+++ root/settings.py
@@ -126,6 +126,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,  # Количество объектов на странице
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
