88b551b78945e0d3edb569004c34e16460dfc42a
islamnurdinov <nurdinovislam4@gmail.com>
2024-11-07 23:46:26 +0600

    daa

--- course/serializers/kaspi.py
+++ course/serializers/kaspi.py
@@ -7,3 +7,15 @@ class PurchaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Purchase
         fields = ['course', 'kaspi', 'payment_status', 'payment_method', 'duration']
+
+    def validate(self, data):
+        user = self.context['request'].user
+        duration = data.get('duration')
+
+        # Проверка, есть ли уже покупка с такой продолжительностью для текущего пользователя
+        if Purchase.objects.filter(user=user, duration=duration).exists():
+            raise serializers.ValidationError(
+                _("Вы уже приобретали курс с такой продолжительностью.")
+            )
+
+        return data
